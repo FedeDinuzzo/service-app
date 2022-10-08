c7c87740e690f37cc907d77e75bdddc41878d4ca
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2022-10-08 15:00:44 -0300

    navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,32 +23,30 @@ const Navbar = () => {
       </ul>
 
       <div className="block fixed bottom-0 w-full z-50 sm:hidden">
-      <div className="flex w-full h-[30px] bg-slate-700">
-        <div className="flex space-evenly items-center absolute bottom-0 left-0 border-b w-[50%] h-[60px] bg-slate-700 pt-[4px]">
-          <Link to="/">
-          <div className={`bg-[${navInicio}] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
-            <div className="font-poppins p-[4px] pb-[8px]">Inicio</div>
-          </Link>
-          <Link to="/heladeras">
-          <div className={`bg-[${navHeladeras}] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
-            <div className="font-poppins p-[4px] pb-[8px]">Heladeras</div>
-          </Link>
-        </div>
+        <div className="flex w-full h-[30px] bg-slate-700">
+          <div className="flex justify-evenly items-center absolute bottom-0 left-6 border-b w-[50%] h-[60px] bg-slate-700 pt-[4px]">
+            <Link to="/">
+              <div className={`bg-[url('./assets/navInicio.svg')] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
+              <div className="font-poppins p-[4px] pb-[8px]">Inicio</div>
+            </Link>
+            <Link to="/heladeras">
+              <div className={`bg-[url('./assets/navHeladeras.svg')] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
+              <div className="font-poppins p-[4px] pb-[8px]">Heladeras</div>
+            </Link>
+          </div>
           <img src={wppLogo} alt="" height="68px" width="68px" className="h-[72px] absolute bottom-[12px] left-0 right-0 m-auto z-[100]"/>
-        <div className="flex space-evenly items-center absolute bottom-0 right-0 border-b w-[50%] h-[60px] bg-slate-700 pt-[4px]">  
-        <Link to="/lavarropas">
-        <div className={`bg-[${navLavarropas}] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
-            <div className="font-poppins p-[4px] pb-[8px]">Lavarropas</div>
-          </Link>
-          <Link to="/contacto">
-            <div className={`bg-[${navContacto}] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
-            <div className="font-poppins p-[4px] pb-[8px]">Contacto</div>
-          </Link>
+          <div className="flex justify-evenly items-center absolute bottom-0 right-0 border-b w-[50%] h-[60px] bg-slate-700 pt-[4px]">  
+            <Link to="/lavarropas">
+              <div className={`bg-[url('./assets/navLavarropas.svg')] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
+              <div className="font-poppins p-[4px] pb-[8px]">Lavarropas</div>
+            </Link>
+            <Link to="/contacto">
+              <div className={`bg-[url('./assets/navContacto.svg')] text-center h-[30px] w-full bg-contain bg-center bg-no-repeat`} />
+              <div className="font-poppins p-[4px] pb-[8px]">Contacto</div>
+            </Link>
+          </div>
         </div>
-      </div>
-    </div>      
-
-     
+      </div>      
     </nav>
     </div>
     </div>
